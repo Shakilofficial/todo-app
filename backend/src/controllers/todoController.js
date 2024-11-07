b799f52f1aeb698f010ba713c91d2681d0c39c72
ShakilOfficial <mrshakilhossainofficial@gmail.com>
2024-11-08 00:36:10 +0600

    get Single todo by id

--- backend/src/controllers/todoController.js
+++ backend/src/controllers/todoController.js
@@ -1,16 +1,34 @@
-const Todo = require('../models/todoModel');
-const { uploadOnCloudinary } = require('../utils/cloudinary');
-
+const Todo = require("../models/todoModel");
+const { uploadOnCloudinary } = require("../utils/cloudinary");
 
 const getAllTodo = async (req, res) => {
-    try {
-      const todos = await Todo.find();
-      res.status(200).json(todos);
-    } catch (error) {
-      res.status(500).json({ error: "Failed to retrieve todos", message: error.message });
+  try {
+    const todos = await Todo.find();
+    res.status(200).json(todos);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "Failed to retrieve todos", message: error.message });
+  }
+};
+
+const getSingleTodo = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const todo = await Todo.findById(id);
+
+    if (!todo) {
+      return res.status(404).json({ error: "Todo not found" });
     }
-  };
- 
+
+    res.status(200).json(todo);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "Failed to retrieve todo", message: error.message });
+  }
+};
+
 const createTodo = async (req, res) => {
   try {
     const {
@@ -51,10 +69,14 @@ const createTodo = async (req, res) => {
     await todo.save();
     res.status(201).json(todo);
   } catch (error) {
-    res.status(500).json({ error: "Failed to create todo", message: error.message });
-    }  
+    res
+      .status(500)
+      .json({ error: "Failed to create todo", message: error.message });
+  }
 };
 
 module.exports = {
-  getAllTodo, createTodo,
+  getAllTodo,
+  getSingleTodo,
+  createTodo,
 };
--- backend/src/routes/todoRoutes.js
+++ backend/src/routes/todoRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const {getAllTodo, createTodo } = require("../controllers/todoController");
+const {getAllTodo,getSingleTodo, createTodo } = require("../controllers/todoController");
 const { upload } = require("../middlewares/uploadMiddleware");
 
 const router = express.Router();
@@ -7,6 +7,7 @@ const router = express.Router();
 router.get("/", (req, res) => res.send("Welcome to Todo App API"));
 
 router.get("/todos", getAllTodo); //GET route for retrieving all todos
+router.get("/todos/:id", getSingleTodo); //GET route for retrieving a single todo
 router.post("/todos", upload.array('attachments'), createTodo); //POST route for creating a todo
 
 module.exports = router;
