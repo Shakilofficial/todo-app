d092f9af1d96f3a65036cdfe9af359a54113861e
ShakilOfficial <mrshakilhossainofficial@gmail.com>
2024-11-08 00:28:11 +0600

    get All Todos controller

--- backend/src/controllers/todoController.js
+++ backend/src/controllers/todoController.js
@@ -1,6 +1,16 @@
 const Todo = require('../models/todoModel');
 const { uploadOnCloudinary } = require('../utils/cloudinary');
 
+
+const getAllTodo = async (req, res) => {
+    try {
+      const todos = await Todo.find();
+      res.status(200).json(todos);
+    } catch (error) {
+      res.status(500).json({ error: "Failed to retrieve todos", message: error.message });
+    }
+  };
+ 
 const createTodo = async (req, res) => {
   try {
     const {
@@ -46,5 +56,5 @@ const createTodo = async (req, res) => {
 };
 
 module.exports = {
-  createTodo,
+  getAllTodo, createTodo,
 };
--- backend/src/routes/todoRoutes.js
+++ backend/src/routes/todoRoutes.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const { createTodo } = require("../controllers/todoController");
+const {getAllTodo, createTodo } = require("../controllers/todoController");
 const { upload } = require("../middlewares/uploadMiddleware");
 
 const router = express.Router();
 
 router.get("/", (req, res) => res.send("Welcome to Todo App API"));
 
-// Add the POST route for creating a todo
-router.post("/todos", upload.array('attachments'), createTodo);
+router.get("/todos", getAllTodo); //GET route for retrieving all todos
+router.post("/todos", upload.array('attachments'), createTodo); //POST route for creating a todo
 
 module.exports = router;
