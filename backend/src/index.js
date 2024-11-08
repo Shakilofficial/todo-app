c1709f031cf1fbaaa188de2822d4a07d956d792f
ShakilOfficial <mrshakilhossainofficial@gmail.com>
2024-11-08 13:54:24 +0600

    done

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,12 +12,14 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN || "http://localhost:3000",
+    origin: process.env.CORS_ORIGIN || "http://localhost:5173",
     credentials: true,
   })
 );
+
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(express.static("public"));
 app.use(cookieParser());
 
 // Routes
